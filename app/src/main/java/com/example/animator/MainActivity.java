5118f54a067f4ff38075498eb1bb84629eccb4b6
masondkl <masondunkle@gmail.com>
2021-06-22 16:17:09 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/animator/MainActivity.java
+++ app/src/main/java/com/example/animator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.animator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,12 +14,14 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     public static final int PIXELS = 16*9;
+    private static MainActivity instance;
     private Pixel[] pixels = new Pixel[PIXELS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instance = this;
 
         fillWhite();
 
@@ -31,14 +35,40 @@ public class MainActivity extends AppCompatActivity {
                 ImageAdapter imageAdapter = new ImageAdapter(MainActivity.this);
 
                 gridView.setAdapter(imageAdapter);
-                gridView.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(getBaseContext(), "Selected grid item " + (position + 1), Toast.LENGTH_SHORT).show());
+                gridView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+                gridView.setOnItemClickListener((parent, view, position, id) -> {
+                    setPixel(position, new Pixel(0, 0, 0));
+                    gridView.setAdapter(imageAdapter);
+                });
             }
         }, 20);
     }
 
+    public void setPixel(int pos, Pixel pixel) {
+        pixels[pos] = pixel;
+    }
+
+    public Pixel getPixel(int pos) {
+        return pixels[pos];
+    }
+
     public void fillWhite() {
         for (int i = 0; i < PIXELS; i++) {
-            pixels[i] = new Pixel(255, 255, 255);
+            pixels[i] = new Pixel(0, 255, 255);
         }
+    }
+
+    public static MainActivity getInstance() {
+        return instance;
     }
 }
