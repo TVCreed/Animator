8aed9bcae841dc405a08d914ee74c5318e610ed5
DWD7 <73203641+DWD7@users.noreply.github.com>
2021-06-28 14:33:50 -0400

    n

--- app/src/main/java/com/example/animator/FrameArray.java
+++ app/src/main/java/com/example/animator/FrameArray.java
@@ -3,6 +3,7 @@ package com.example.animator;
 import java.util.ArrayList;
 
 public class FrameArray {
+    private int framePos = 0;
     ArrayList<Frame> Frames = new ArrayList<>(10);
 
     public void AddFrame(Frame f) {
